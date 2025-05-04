f6ce9e4bd18d1d5d5caddd4b08e96a79511079e8
dripboishop <dripboy.clothing@gmail.com>
2025-05-04 23:52:05 +0200

    Delete search.js

--- search.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const input = document.getElementById('searchInput');
-  input.addEventListener('input', () => {
-    console.log('Searching:', input.value);
-  });
-});
